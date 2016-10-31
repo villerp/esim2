989edae7ee0b1eaa9ea32469f095ed89db3b82a3
Unknown <ville.rp@hotmail.com>
2016-10-31 10:33:29 +0200

    Get-lomake luotu ja valmis

---
+++ get.php
@@ -0,0 +1,18 @@
+<h1>Testataan get-metodia</h1>
+<form method="get" action="get.php">
+<label>Etunimi</label>
+<input type="text" name="en" required="">
+<br>
+<label>Sukunimi</label>
+<input type="text" name="sn" required="">
+<br>
+<input type="submit" name="nappi" value="Lähetä">
+</form>
+
+<?php
+if (isset($_GET['nappi'])) {
+$enimi=$_GET['en'];
+$snimi=$_GET['sn'];
+echo "Terve $enimi $snimi";
+}
+?>
